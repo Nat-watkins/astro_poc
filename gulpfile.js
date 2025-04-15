8d10e08f07e7f12f41407115c94f8d0fb78795b0
Jonathan Hutchison <jonathanhutchison@navapbc.com>
2025-04-15 13:50:17 -0400

    Move scss to assets

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const uswds = require("@uswds/compile");
 uswds.settings.version = 3;
 
 /** Paths **/
-uswds.paths.dist.img = "./src/assets/uswds/images"
+uswds.paths.dist.img = "./src/assets/uswds/img"
 uswds.paths.dist.fonts = "./src/assets/uswds/fonts"
 uswds.paths.dist.js = "./src/assets/uswds/js"
 uswds.paths.dist.css = "./src/assets/uswds/css"
