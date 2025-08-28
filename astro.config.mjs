2ca2e77388796bc2488dcc4004cb6f11ab5c7688
Natalie Watkins <nataliewatkins@navapbc.com>
2025-08-28 16:46:36 -0400

    make button component

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bcda.cms.gov',
+  site: 'https://bluebutton.cms.gov',
 
   vite: {
     css: {
